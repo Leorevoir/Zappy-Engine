b41178a6d7710c9d19df0f39b9675ae0a7d81411
Leorevoir <leo.quinzler@epitech.eu>
2025-05-19 21:54:34 +0200

    add: pre-processor to avoid heavy includes

--- include/GLEngineTypes.hpp
+++ include/GLEngineTypes.hpp
@@ -6,11 +6,29 @@
 * WARN: **always** include `<glad/gl.h>` **before** `<GLFW/glfw3.h>`
 */
 
+/**
+* INFO: avoid including opengl headers in every files that actually still need zap engine types
+*/
+#if !defined(ZAP_TYPES_ONLY)
 // clang-format off
+#define GLAD_GL_IMPLEMENTATION
 #include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
 // clang-format on
-
 namespace zap {
 using WindowPtr = GLFWwindow *;
-}
+}// namespace zap
+#endif
+
+namespace zap {
+
+using u8 = unsigned char;
+using u16 = unsigned short;
+using u32 = unsigned int;
+using u64 = unsigned long long;
+
+using f32 = float;
+using f64 = double;
+
+}// namespace zap
