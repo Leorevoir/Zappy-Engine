eea147d21f1810562ca4b75e9e132a19ecf3c57d
Leorevoir <leo.quinzler@epitech.eu>
2025-05-19 21:54:44 +0200

    add: epitech's header to main

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,15 +2,14 @@
 ** EPITECH PROJECT, 2025
 ** Zappy
 ** File description:
-** Main.cpp
+** Application.cpp
 */
 
 #include "Engine/Core/Application.hpp"
-#include "Macro.hpp"
 
 int main(void)
 {
     zap::core::Application::getInstance().run();
 
-    return SUCCESS;
+    return 0;
 }
