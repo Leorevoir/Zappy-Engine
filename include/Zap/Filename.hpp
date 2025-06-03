4b18679fba59d3aae1111e85b912c11d9d5f7a78
Leorevoir <leo.quinzler@epitech.eu>
2025-06-03 17:22:54 +0200

    add: ZapFilename

--- include/Zap/Filename.hpp
+++ include/Zap/Filename.hpp
@@ -8,11 +8,13 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace zap::Filename {
 
 [[nodiscard]] std::string getPath(const std::string &name);
 [[nodiscard]] bool exists(const std::string &path);
 [[nodiscard]] std::string getCurrentDir();
+[[nodiscard]] std::vector<std::string> getLines(const std::string &path);
 
 }// namespace zap::Filename
--- src/Zap/Utils/Filename.cpp
+++ src/Zap/Utils/Filename.cpp
@@ -5,13 +5,15 @@
 ** Filename.cpp
 */
 
+#include <Zap/Error.hpp>
 #include <Zap/Filename.hpp>
+
 #include <filesystem>
+#include <fstream>
 
 std::string zap::Filename::getPath(const std::string &name)
 {
-    const std::filesystem::path relative = "assets/models/" + name + ".obj";
-    const std::filesystem::path absolute = std::filesystem::absolute(relative);
+    const std::filesystem::path absolute = std::filesystem::absolute(name);
 
     return absolute.string();
 }
@@ -24,4 +26,26 @@ bool zap::Filename::exists(const std::string &path)
 std::string zap::Filename::getCurrentDir()
 {
     return std::filesystem::current_path().string();
+}
+
+std::vector<std::string> zap::Filename::getLines(const std::string &path)
+{
+    if (!exists(path)) {
+        throw exception::Error("zap::Filename::getLines", "file: ", path, " does not exist!");
+    }
+
+    std::ifstream file(path);
+
+    if (!file.is_open()) {
+        throw exception::Error("zap::Filename::getLines", "cannot open file: ", path);
+    }
+
+    std::vector<std::string> lines;
+    std::string line;
+
+    while (std::getline(file, line)) {
+        lines.push_back(std::move(line));
+    }
+
+    return lines;
 }
